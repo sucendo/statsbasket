062fce3a64f7c8d2113100ca25cf73524e2ec540
sucendo <samuelug.pro@gmail.com>
2022-09-06 15:22:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get('/pizarra-tactica-baloncesto', function(request, response) {
   response.render('pages/pizarra-tactica-baloncesto');
 });
 
+app.get('/mapa', function(request, response) {
+  response.render('pages/map');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
